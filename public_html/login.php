e6eb738ecdf212f1b5f62d9414f1947cfdd2f3aa
alixlm19 <alixlm19@hotmail.com>
2020-01-22 11:58:16 -0500

    created basic login/registration form

---
+++ public_html/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Fullstack Assessment</title>
+</head>
+<body>
+    <div id="login_box">
+        <form method="POST">
+            <input type="text" placeholder="Email">
+            <input type="password" placeholder="Enter your password">
+            <input type="submit" value="Login">
+        </form>
+        <span>Not registered?<a href="register.php">Sign up here</a></span>
+    </div>
+</body>
+
+</html>
---
+++ public_html/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Fullstack Assessment</title>
+</head>
+<body>
+    <div id="login_box">
+        <form method="POST">
+            <input type="text" placeholder="First name"></br>
+            <input type="text" placeholder="Last name"></br>
+
+            <input type="password" placeholder="Password"></br>
+            <input type="password" placeholder="Confirm password"></br>
+
+            <input type="submit" value="Login"></br>
+        </form>
+
+        <span>Already have an account?<a href="login.php">Log in</a></span>
+
+    </div>
+</body>
+
+</html>
